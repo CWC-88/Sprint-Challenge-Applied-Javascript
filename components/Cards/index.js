ae10cf30978891f3b155cc21dc8b386fcc13c87b
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-02-23 17:45:38 -0600

    aspiduhfpiufhasioufhsaf

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,14 +6,74 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
-//   </div>
-// </div>
+/* <div class="card">
+  <div class="headline">{Headline of article}</div>
+  <div class="author">
+    <div class="img-container">
+      <img src={url of authors image} />
+    </div>
+    <span>By {authors name}</span>
+  </div>
+</div> */
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+
+
+
+const cardcontainer = document.querySelector('.cards-container');
+const dogArray =['https://lambda-times-backend.herokuapp.com/articles']
+
+function createCarddata(dog){
+
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response)=>{
+        const data = response.data;
+        const newCard = dogCardCreator(data);
+        cardcontainer.appendChild(newCard)
+    })
+
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+dogArray.forEach((dog) => {
+    createCarddata(dog)
+  })
+
+
+
+
+function dogCardCreator(obj){
+    const card = document.createElement('div')
+   
+   
+    const hl = document.createElement('div')
+    const author = document.createElement('div')
+    const imagecontainer = document.createElement('div')
+    const imageiteself = document.createElement('img')
+    const authorname = document.createElement('span')
+
+card.appendChild(hl)
+card.appendChild(author)
+author.appendChild(imagecontainer)
+imagecontainer.appendChild(imageiteself)
+author.appendChild(authorname)
+
+card.classList.add('card')
+hl.classList.add('headline')
+hl.textContent ="ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects"
+author.classList.add('author')
+imagecontainer.classList.add('img-container')
+imageiteself.src = './assets/sir.jpg'
+authorname.textContent = 'SIR RUFF N STUFF'
+
+return card
+}
+
+
+
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const headerContainer = document.createElement('div');
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+    
+    headerContainer.classList.add('header')
+    date.classList.add('date')
+    title.classList.add('title')
+    temp.classList.add('temp')
+    
+    date.textContent = 'Feb 21 2020';
+    title.textContent = 'Lambda Times';
+    temp.textContent ='98'
+    
+    headerContainer.appendChild(date);
+    headerContainer.appendChild(title);
+    headerContainer.appendChild(temp);
+    
+    return headerContainer
+    
+    
+    
+    }
+    
+    
+    
+    const parentHeader = document.querySelector('.header-container');
+    parentHeader.appendChild(Header());
+    
